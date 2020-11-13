700b9f909ef392279872830fad32d4173948f8e6
wiztemple <wiztemple7@gmail.com>
2020-11-13 09:16:39 +0100

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,13 @@
-
 const respositoryContent = document.getElementById('repositoryContent');
-function timeSince(date) {
+const GITHUB_TOKEN = "7abb2a3585272c45070f26c852de7c4415fdbbca";
 
-  var seconds = Math.floor((new Date() - date) / 1000);
-
-  var interval = seconds / 31536000;
-
-  if (interval > 1) {
-    return Math.floor(interval) + " years";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-}
-var aDay = 24 * 60 * 60 * 1000;
-console.log(timeSince(new Date(Date.now() - aDay)));
-console.log(timeSince(new Date(Date.now() - aDay * 2))); ``
 fetch('https://api.github.com/graphql', {
   method: 'POST',
   body: JSON.stringify({
     query: `query { viewer{ repositories(last:20) { totalCount, nodes{nameWithOwner, name, description, forkCount, updatedAt, viewerHasStarred, 
-    licenseInfo {id, name}}, pageInfo {endCursor, hasNextPage}} } }` }),
+      primaryLanguage{id, name}, licenseInfo {id, name}}, pageInfo {endCursor, hasNextPage}} } }` }),
   headers: {
-    Authorization: `Bearer baced869f22d50f0fce86f046372de2233a97c47`,
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
     'Content-Type': 'application/json',
   },
 })
@@ -51,24 +22,21 @@ fetch('https://api.github.com/graphql', {
       </h3>
       <span></span>
       <div class="repository-details">
-        <span class="repository-details-child">
-          <span class="language-color html"></span>
-          HTML
-        </span>
+        
         <span class="repository-details-child">
           <span class="language-color js"></span>
-          JavaScript
+          ${repository.primaryLanguage && repository.primaryLanguage.name}
         </span>
         <span class="repository-details-child starCount">
           <span>
-            <svg aria-label="star" class="octicon octicon-star" viewBox="0 0 16 16" version="1.1"
+            <svg aria-label="star" viewBox="0 0 16 16" version="1.1"
               width="16" height="16" role="img">
               <path fill-rule="evenodd"
                 d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z">
               </path>
             </svg>
           </span>
-          3
+          ${repository && repository.stargazerCount > 0 ? repository.stargazerCount : ""}
         </span>
         <span class="repository-details-child forkCount">
           <span>
@@ -80,17 +48,6 @@ fetch('https://api.github.com/graphql', {
             </svg>
           </span>
           5
-        </span>
-        <span class="repository-details-child">
-          <span>
-            <svg class="" viewBox="0 0 16 16" version="1.1" width="16" height="16"
-              aria-hidden="true">
-              <path fill-rule="evenodd"
-                d="M8.75.75a.75.75 0 00-1.5 0V2h-.984c-.305 0-.604.08-.869.23l-1.288.737A.25.25 0 013.984 3H1.75a.75.75 0 000 1.5h.428L.066 9.192a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.514 3.514 0 00.686.45A4.492 4.492 0 003 11c.88 0 1.556-.22 2.023-.454a3.515 3.515 0 00.686-.45l.045-.04.016-.015.006-.006.002-.002.001-.002L5.25 9.5l.53.53a.75.75 0 00.154-.838L3.822 4.5h.162c.305 0 .604-.08.869-.23l1.289-.737a.25.25 0 01.124-.033h.984V13h-2.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-2.5V3.5h.984a.25.25 0 01.124.033l1.29.736c.264.152.563.231.868.231h.162l-2.112 4.692a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.517 3.517 0 00.686.45A4.492 4.492 0 0013 11c.88 0 1.556-.22 2.023-.454a3.512 3.512 0 00.686-.45l.045-.04.01-.01.006-.005.006-.006.002-.002.001-.002-.529-.531.53.53a.75.75 0 00.154-.838L13.823 4.5h.427a.75.75 0 000-1.5h-2.234a.25.25 0 01-.124-.033l-1.29-.736A1.75 1.75 0 009.735 2H8.75V.75zM1.695 9.227c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327l-1.305 2.9zm10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327l-1.305 2.9z">
-              </path>
-            </svg>
-          </span>
-          ${repository.licenseInfo && repository.licenseInfo.name}
         </span>
         <span class="repository-details-child">Updated 2 hours ago</span>
       </div>
